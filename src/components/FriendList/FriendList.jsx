e29a1cb76c92e7c5ecb4c83092940acb0655e8ed
MOkhten <m.okhten@gmail.com>
2022-12-15 12:55:41 +0200

    friends

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import css from './FriendList.module.css'
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className='friend-list'>
+    <ul className={ css.friend_list}>
       {friends.map(friend => {
         return (
           <FriendListItem
             key={friend.id}
             name={friend.name}
-            status={friend.isOnline}
+            isOnline={friend.isOnline}
             avatar={friend.avatar}
           />
        );
